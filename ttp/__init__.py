5f31c8ccc265c75b29f2ad3e4e431a5f83b3a1f5
jluby127 <50976985+jluby127@users.noreply.github.com>
2025-11-12 09:42:12 -0800

    Comment out import print statement
    Commented out the print statement for module import.

--- ttp/__init__.py
+++ ttp/__init__.py
@@ -1,6 +1,6 @@
 import os
 import sys
-print("Importing all of the TTP modules")
+# print("Importing all of the TTP modules")
 
 _ROOT = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(_ROOT)
